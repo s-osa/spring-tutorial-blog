600f124c85ffacf5d8fd2e7ddcb920a40f129757
Shunsuke Osa <hhelibebcnofnenamg@gmail.com>
2023-05-31 00:32:20 +0900

    Initialize the project

---
+++ src/main/kotlin/com/example/springtutorialblog/SpringTutorialBlogApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springtutorialblog
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringTutorialBlogApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringTutorialBlogApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/springtutorialblog/SpringTutorialBlogApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.springtutorialblog
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringTutorialBlogApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
